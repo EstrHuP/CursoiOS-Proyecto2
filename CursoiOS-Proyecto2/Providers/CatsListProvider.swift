d1f80e53014a8c80f796093a3f9a83fe3a818599
EstrHuP <esthrhup@gmail.com>
2022-01-27 18:31:31 +0100

    Base URL

--- CursoiOS-Proyecto2/Providers/CatsListProvider.swift
+++ CursoiOS-Proyecto2/Providers/CatsListProvider.swift
@@ -21,16 +21,15 @@ class NetworkCatsListProvider: CatsListProviderContract {
     var cats = [Cat]()
     
     private let session: Session
+    private let bundle: Bundle
     
-    init(session: Session = .default) {
+    init(session: Session = .default, bundle: Bundle = .main) {
         self.session = session
+        self.bundle = bundle
     }
     
     func getCatsList(_ completion: @escaping (Result<[Cat], CatsListProviderError>) -> ()) {
-        guard let url = URL(string: "https://cataas.com/api/cats") else {
-            completion(.failure(.badUrl))
-            return
-        }
+        let url = Bundle.main.baseAPIUrl.appendingPathComponent("api/cats")
         
         let request = URLRequest(url: url)
         session.request(request).responseDecodable { [weak self] (response: DataResponse<[Cat], AFError>) in
